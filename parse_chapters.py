2b3d70f22b1181ac2d5e98238c5734045f3af4d4
DerrickStuckey <derrickstuckey@gmail.com>
2015-05-15 00:17:20 -0400

    working classifier

--- parse_chapters.py
+++ parse_chapters.py
@@ -8,10 +8,11 @@ class Chapter:
         self.content = content
 
 #book_filename = 'raw_text/#1A Game of Thrones.txt'
-book_filename = 'raw_text/#2A Clash of Kings.txt'
+#book_filename = 'raw_text/#2A Clash of Kings.txt'
 #book_filename = 'raw_text/#3A Storm of Swords.txt'
 #book_filename = 'raw_text/#4A Feast for Crows.txt'
 #book_filename = 'raw_text/#5A Dance With Dragons.txt'
+book_filename = 'raw_text/#5A Dance With Dragons cleaned.txt'
 
 def parse_chapters(book_filename):
     chapter_names = []
@@ -20,13 +21,16 @@ def parse_chapters(book_filename):
     with open(book_filename) as book:
         current_chapter = ""
         for line in book.readlines():
+            #remove non-ascii characters
+            line = ''.join(i for i in line if ord(i)<128)
+
             #break at appendix
-            if ("APPENDIX" in line):
+            if ("APPENDIX" in line or "EPILOGUE" in line):
                 break
 
             if (line.isupper() and not (" " in line.strip())):
                 # print line
-                chapter_names.append(line)
+                chapter_names.append(line.strip())
                 chapters.append(current_chapter)
                 current_chapter = ""
             else:
@@ -41,7 +45,7 @@ def parse_chapters(book_filename):
 
     return [Chapter(z[0],z[1]) for z in zipped]
 
-chaps = parse_chapters(book_filename)
-for chap in chaps:
-    print chap.name
-    print chap.content[0:20]
+# chaps = parse_chapters(book_filename)
+# for chap in chaps:
+#     print "Chapter: ", chap.name
+#     print chap.content[0:50]

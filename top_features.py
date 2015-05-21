3c84061e6f0aa68e3c16de4c10c60b0b9825efe9
DerrickStuckey <derrickstuckey@gmail.com>
2015-05-21 14:42:29 -0400

    added csv output

--- top_features.py
+++ top_features.py
@@ -2,11 +2,7 @@ __author__ = 'dstuckey'
 
 import parse_chapters
 import prediction_utils as pu
-
-from sklearn.metrics import classification_report
-from sklearn.metrics import precision_score
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import recall_score
+import csv
 
 book_filenames = ['raw_text/#1A Game of Thrones.txt',
                   'raw_text/#2A Clash of Kings.txt',
@@ -16,6 +12,16 @@ book_filenames = ['raw_text/#1A Game of Thrones.txt',
 
 chapters = []
 
+features_dir = "./results/features"
+
+def write_narrator_features(narrator, weighted_features):
+    output_filename = features_dir + "/" + narrator + "_features.csv"
+    with open(output_filename, 'w') as output_file:
+        writer = csv.writer(output_file)
+        writer.writerow(['feature','weight'])
+        for weighted_feature in weighted_features:
+            writer.writerow(weighted_feature)
+
 # narrators = ['ARYA','JON','SANSA','TYRION','DAENERYS','CERSEI','JAIME']
 narrators = ['ARYA','JON','SANSA','TYRION','DAENERYS','CERSEI','JAIME','BRAN','DAVOS','THEON','SAMWELL','EDDARD','BRIENNE']
 
@@ -23,22 +29,23 @@ for book_filename in book_filenames:
     cur_chapters = parse_chapters.parse_chapters(book_filename)
     chapters = chapters + cur_chapters
 
+# since we are only interested in the features used and their weights after training,
+# we use the full dataset as training data, and a throwaway test set
 train_chapters, test_chapters = pu.partition(chapters, test_prop=0, randomize=True)
 contents_train = [x.content for x in train_chapters]
-contents_test = [x.content for x in test_chapters]
+contents_test = ["these","are","not","real","chapters"]
 
-# run for several narrators:
+# run for all narrators:
 for narrator in narrators:
-    print "\n\n"
-    print "Narrator: ", narrator
     y_train = pu.extract_y(train_chapters, narrator)
     y_test = pu.extract_y(test_chapters, narrator)
 
-    # use full data set to train (and to test, although test predictions are ignored)
-    preds, selected_features, clf = pu.svm_classify(contents_train, y_train, contents_train, k="all")
+    # SVM classifier
+    preds, selected_features, clf = pu.svm_classify(contents_train, y_train, contents_test, k="all")
     coefs = clf.coef_.toarray()
 
-    # preds, selected_features, clf = pu.nb_classify(contents_train, y_train, contents_train,k="all")
+    # Naive Bayes classifier
+    # preds, selected_features, clf = pu.nb_classify(contents_train, y_train, contents_test,k="all")
     # coefs = clf.feature_log_prob_
 
     # print "coefs len: ", len(coefs)
@@ -46,18 +53,15 @@ for narrator in narrators:
 
     class_1_coefs = coefs[-1]
 
-    # don't care about classification report
-    # print(classification_report(y_test, preds))
-
-    # print "selected features: "
-    # # print selected_features
-    # for (feature,coef) in zip(selected_features,class_1_coefs):
-    #     print str(feature), ": ", coef
+    print "\n\n"
+    print "Narrator: ", narrator
 
     weighted_features = zip(selected_features,class_1_coefs)
     sorted_features = sorted(weighted_features, key=lambda x: x[1], reverse=True)
-    print "top positive features: "
+    print "highest weighted features: "
     print sorted_features[:10]
 
-    print "top negative features: "
+    print "lowest weighted features: "
     print [x for x in sorted_features if x[1] < 0][-10:]
+
+    write_narrator_features(narrator, sorted_features)
